36cbf1fb5f8ed57fc25d676e9e53ccda1d92e20c
jhon.cordoba <jhon.cordoba@smartquick.com.co>
2021-11-25 18:36:51 -0500

    fix view

--- base/views.py
+++ base/views.py
@@ -37,5 +37,5 @@ def getProducts(request):
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer.get(product, many=False)
+    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
