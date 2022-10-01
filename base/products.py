ae188e98d20557d33b09cef8f3931a7012ab63bb
alejandro.cordoba <alejo.artia@gmail.com>
2022-10-01 23:32:11 +0200

    add: little change

--- base/products.py
+++ base/products.py
@@ -4,7 +4,8 @@ products = [
     'name': 'Airpods Wireless Bluetooth Headphones',
     'image': '/images/airpods.jpg',
     'description':
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers '
+      'immersive listening experience Built-in microphone allows you to take calls while working',
     'brand': 'Apple',
     'category': 'Electronics',
     'price': 89.99,
@@ -17,7 +18,8 @@ products = [
     'name': 'iPhone 11 Pro 256GB Memory',
     'image': '/images/phone.jpg',
     'description':
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without '
+      'complexity. An unprecedented leap in battery life',
     'brand': 'Apple',
     'category': 'Electronics',
     'price': 599.99,
@@ -30,7 +32,8 @@ products = [
     'name': 'Cannon EOS 80D DSLR Camera',
     'image': '/images/camera.jpg',
     'description':
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust '
+      'focusing systems and an intuitive design',
     'brand': 'Cannon',
     'category': 'Electronics',
     'price': 929.99,
@@ -43,7 +46,8 @@ products = [
     'name': 'Sony Playstation 4 Pro White Version',
     'image': '/images/playstation.jpg',
     'description':
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, '
+      'television, music',
     'brand': 'Sony',
     'category': 'Electronics',
     'price': 399.99,
@@ -56,7 +60,8 @@ products = [
     'name': 'Logitech G-Series Gaming Mouse',
     'image': '/images/mouse.jpg',
     'description':
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons '
+      'allow customization for a smooth playing experience',
     'brand': 'Logitech',
     'category': 'Electronics',
     'price': 49.99,
@@ -69,7 +74,8 @@ products = [
     'name': 'Amazon Echo Dot 3rd Generation',
     'image': '/images/alexa.jpg',
     'description':
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that '
+      'fits perfectly into small space',
     'brand': 'Amazon',
     'category': 'Electronics',
     'price': 29.99,
@@ -82,7 +88,8 @@ products = [
     'name': 'Airpods Wireless Bluetooth Headphones',
     'image': '/images/airpods.jpg',
     'description':
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers '
+      'immersive listening experience Built-in microphone allows you to take calls while working',
     'brand': 'Apple',
     'category': 'Electronics',
     'price': 89.99,
@@ -95,7 +102,8 @@ products = [
     'name': 'iPhone 11 Pro 256GB Memory',
     'image': '/images/phone.jpg',
     'description':
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without '
+      'complexity. An unprecedented leap in battery life',
     'brand': 'Apple',
     'category': 'Electronics',
     'price': 599.99,
@@ -108,7 +116,8 @@ products = [
     'name': 'Cannon EOS 80D DSLR Camera',
     'image': '/images/camera.jpg',
     'description':
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust '
+      'focusing systems and an intuitive design',
     'brand': 'Cannon',
     'category': 'Electronics',
     'price': 929.99,
@@ -121,7 +130,8 @@ products = [
     'name': 'Sony Playstation 4 Pro White Version',
     'image': '/images/playstation.jpg',
     'description':
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, '
+      'television, music',
     'brand': 'Sony',
     'category': 'Electronics',
     'price': 399.99,
@@ -134,7 +144,8 @@ products = [
     'name': 'Logitech G-Series Gaming Mouse',
     'image': '/images/mouse.jpg',
     'description':
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons '
+      'allow customization for a smooth playing experience',
     'brand': 'Logitech',
     'category': 'Electronics',
     'price': 49.99,
@@ -147,7 +158,8 @@ products = [
     'name': 'Amazon Echo Dot 3rd Generation',
     'image': '/images/alexa.jpg',
     'description':
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that '
+      'fits perfectly into small space',
     'brand': 'Amazon',
     'category': 'Electronics',
     'price': 29.99,
